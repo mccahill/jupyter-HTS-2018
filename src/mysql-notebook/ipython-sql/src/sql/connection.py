ada7a8f61c73e361716fbde20966a295845a2f73
Mark McCahill <mark.mccahill@duke.edu>
2015-11-19 14:28:05 -0500

    include the ipython-sql code
    Signed-off-by: Mark McCahill <mark.mccahill@duke.edu>

---
+++ src/mysql-notebook/ipython-sql/src/sql/connection.py
@@ -0,0 +1,46 @@
+import sqlalchemy
+
+class Connection(object):
+    current = None
+    connections = {}
+    @classmethod
+    def tell_format(cls):
+        return "Format: (postgresql|mysql)://username:password@hostname/dbname, or one of %s" \
+               % str(cls.connections.keys())
+    def __init__(self, connect_str=None):
+        try:
+            engine = sqlalchemy.create_engine(connect_str)
+        except: # TODO: bare except; but what's an ArgumentError?
+            print(self.tell_format())
+            raise 
+        self.dialect = engine.url.get_dialect()
+        self.metadata = sqlalchemy.MetaData(bind=engine)
+        self.name = self.assign_name(engine)
+        self.session = engine.connect() 
+        self.connections[self.name] = self
+        self.connections[str(self.metadata.bind.url)] = self
+        Connection.current = self
+    @classmethod
+    def get(cls, descriptor):
+        if isinstance(descriptor, Connection):
+            cls.current = descriptor
+        elif descriptor:
+            conn = cls.connections.get(descriptor) or \
+                   cls.connections.get(descriptor.lower()) 
+            if conn:
+                cls.current = conn
+            else:
+                cls.current = Connection(descriptor)
+        if cls.current:
+            return cls.current
+        else:
+            raise Exception(cls.tell_format())
+    @classmethod
+    def assign_name(cls, engine):
+        core_name = '%s@%s' % (engine.url.username, engine.url.database)
+        incrementer = 1
+        name = core_name
+        while name in cls.connections:
+            name = '%s_%d' % (core_name, incrementer)
+            incrementer += 1
+        return name
